24eb7851dca42b224b340a28d3ba66abf0c14888
Veraprot <vera.protopopova@gmail.com>
2019-01-08 18:10:28 -0500

    idk

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,14 +1,11 @@
 require 'pry'
 
 class Api::V1::GamesController < ApplicationController
-  @@cards = []
-
   def create_timeline 
     earliest_record = Card.minimum(:date)
     latest_record = Card.maximum(:date)
-    binding.pry
 
-    if Card.all.length > 0 || game_params[:startDate] < earliest_record || game_params[:endDate] > latest_record 
+    if game_params[:startDate] < earliest_record || game_params[:endDate] > latest_record 
       HistoryAPI.process_request(game_params[:startDate], game_params[:endDate])
     end 
 
