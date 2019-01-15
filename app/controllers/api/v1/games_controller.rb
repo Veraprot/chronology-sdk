deaf6e12e5dc4ef7dbc696e2ef1da3d358644f20
Veraprot <vera.protopopova@gmail.com>
2019-01-15 18:41:03 -0500

    add active players

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::GamesController < ApplicationController
   def create_timeline 
     earliest_record = Card.minimum(:date)
     latest_record = Card.maximum(:date)
+    start_date = game_params[:start_date].to_i
+    end_date = game_params[:end_date].to_i
 
     if Card.all.length == 0
       HistoryAPI.process_request(game_params[:start_date], game_params[:end_date]) 
@@ -12,8 +14,9 @@ class Api::V1::GamesController < ApplicationController
       HistoryAPI.process_request(game_params[:start_date], earliest_record.to_s) 
       HistoryAPI.process_request(game_params[:end_date], latest_record.to_s) 
     end 
-    
-    @game = Game.new(game_params)
+    user = current_user
+    @game = Game.create(game_params)
+    Participant.create(user_id: current_user.id, game_id: @game.id, score: 0)
     @cards = Card.where("date > ? AND date < ?", start_date, end_date)
     
     render json: @cards, status: :created
