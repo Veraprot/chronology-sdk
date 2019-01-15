283253e686f5fc82dcf26e03ebe1ab834f11cb19
Veraprot <vera.protopopova@gmail.com>
2019-01-15 18:26:55 -0500

    delete r logic

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,16 +2,11 @@ require 'pry'
 
 class Api::V1::GamesController < ApplicationController
   # skip_before_action :authorized, only: [:create_timeline]
-  @@date_range = {}
   def create_timeline 
     earliest_record = Card.minimum(:date)
     latest_record = Card.maximum(:date)
-    start_date = game_params[:start_date].to_i
-    end_date = game_params[:end_date].to_i
 
     if Card.all.length == 0
-      @@date_range["start"] = start_date
-      @@date_range["end"] = end_date
       HistoryAPI.process_request(game_params[:start_date], game_params[:end_date]) 
     elsif start_date < earliest_record || end_date > latest_record 
       HistoryAPI.process_request(game_params[:start_date], earliest_record.to_s) 
