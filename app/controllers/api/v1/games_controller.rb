1aab108a5111544235fc4980eb1d2ce3b9758c69
Veraprot <vera.protopopova@gmail.com>
2019-01-15 18:14:44 -0500

    limit querying range

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Api::V1::GamesController < ApplicationController
   # skip_before_action :authorized, only: [:create_timeline]
+  @@date_range = {}
   def create_timeline 
     earliest_record = Card.minimum(:date)
     latest_record = Card.maximum(:date)
@@ -9,11 +10,12 @@ class Api::V1::GamesController < ApplicationController
     end_date = game_params[:end_date].to_i
 
     if Card.all.length == 0
+      @@date_range["start"] = start_date
+      @@date_range["end"] = end_date
       HistoryAPI.process_request(game_params[:start_date], game_params[:end_date]) 
-    end 
-    
-    if start_date < earliest_record || end_date > latest_record 
-      HistoryAPI.process_request(game_params[:start_date], game_params[:end_date])
+    elsif start_date < earliest_record || end_date > latest_record 
+      HistoryAPI.process_request(game_params[:start_date], earliest_record.to_s) 
+      HistoryAPI.process_request(game_params[:end_date], latest_record.to_s) 
     end 
     
     @game = Game.new(game_params)
